0335f0af85d13e22952101e8d58a5a61d3926046
Manash Kumar Mandal <manash@protonmail.com>
2017-05-02 00:15:33 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var database = require('./config/database');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
